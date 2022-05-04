85ba7585eb3fd5f9b65028962912b6a5eec324b6
Mark Wainwright <wainwrightml@gmail.com>
2022-05-04 19:51:33 +0100

    Changed Some numbers

--- GridPuzzles/Session/SessionSettings.cs
+++ GridPuzzles/Session/SessionSettings.cs
@@ -7,7 +7,7 @@ public class SessionSettings
     public bool GoToFinalStateOnKeyPress { get; set; } = false;
     public bool SingleStep { get; set; } = false;
     public int MinimumValuesToShow { get; set; } = 8;
-    public int MaxFinalIntervalMS { get; set; } = 500;
+    public int MaxFinalIntervalMS { get; set; } = 100;
 
     public TimeSpan MaxFinalInterval =>  TimeSpan.FromMilliseconds(MaxFinalIntervalMS);
         
