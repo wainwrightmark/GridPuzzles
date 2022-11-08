f770e481862e42c494ae4143310bfbc33fffa1fb
Mark Wainwright <wainwrightml@gmail.com>
2022-11-08 09:44:54 +0000

    Updated packages

--- Words/DictionaryHelper.cs
+++ Words/DictionaryHelper.cs
@@ -9,17 +9,15 @@ namespace Words;
 
 public class DictionaryHelper
 {
-
     private static Stream StringToStream(string s)
     {
-        var byteArray = Encoding.ASCII.GetBytes( s );
-        var stream = new MemoryStream( byteArray );
+        var byteArray = Encoding.ASCII.GetBytes(s);
+        var stream = new MemoryStream(byteArray);
         return stream;
     }
 
     public DictionaryHelper()
     {
-
         HunspellWordList = new Lazy<WordList>(() =>
         {
             using var dictionaryStream = StringToStream(Resources.index1);
@@ -30,24 +28,24 @@ public class DictionaryHelper
             return wl;
         });
 
-        AllWords = new Lazy<IReadOnlyCollection<string>>(()=>
+        AllWords = new Lazy<IReadOnlyCollection<string>>(() =>
             GenerateAllWords(HunspellWordList.Value)
                 .Distinct()
-                .Where(x=>x.All(char.IsLetter) && x.Length > 2).ToList());
+                .Where(x => x.All(char.IsLetter) && x.Length > 2).ToList());
 
-        NormalWords = new Lazy<IReadOnlyCollection<string>>(()=>
-            AllWords.Value.Where(x=>x.All(char.IsLower)).ToList());
+        NormalWords = new Lazy<IReadOnlyCollection<string>>(() =>
+            AllWords.Value.Where(x => x.All(char.IsLower)).ToList());
 
-        ProperNouns = new Lazy<IReadOnlyCollection<string>>(()=>
-            AllWords.Value.Where(x=>x.Any(char.IsLower) && x.Any(char.IsUpper)).ToList());
+        ProperNouns = new Lazy<IReadOnlyCollection<string>>(() =>
+            AllWords.Value.Where(x => x.Any(char.IsLower) && x.Any(char.IsUpper)).ToList());
 
-        Acronyms = new Lazy<IReadOnlyCollection<string>>(()=>
-            AllWords.Value.Where(x=>x.All(char.IsUpper)).ToList());
+        Acronyms = new Lazy<IReadOnlyCollection<string>>(() =>
+            AllWords.Value.Where(x => x.All(char.IsUpper)).ToList());
 
         MostCommonWords = new Lazy<IReadOnlyCollection<string>>(
-            ()=>
-                Resources.MostCommonWords.Split("\r\n", StringSplitOptions.RemoveEmptyEntries )
-                    .Select(x=>x.Trim()).Distinct(StringComparer.OrdinalIgnoreCase).ToList());
+            () =>
+                Resources.MostCommonWords.Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim()).Distinct(StringComparer.OrdinalIgnoreCase).ToList());
     }
 
     public readonly Lazy<WordList> HunspellWordList;
@@ -70,7 +68,8 @@ public class DictionaryHelper
         var wordEntryDetail = HunspellWordList.Value[word];
 
         var allPrefixesForWord =
-            HunspellWordList.Value.Affix.Prefixes.Where(p => wordEntryDetail.Any(x => x.ContainsFlag(p.AFlag))).ToList();
+            HunspellWordList.Value.Affix.Prefixes.Where(p => wordEntryDetail.Any(x => x.ContainsFlag(p.AFlag)))
+                .ToList();
 
         string combined;
         foreach (var prefixEntry in allPrefixesForWord.SelectMany(p => p.Entries))
@@ -78,17 +77,20 @@ public class DictionaryHelper
                 yield return combined;
 
         var allSuffixesForWord =
-            HunspellWordList.Value.Affix.Suffixes.Where(s => wordEntryDetail.Any(x => x.ContainsFlag(s.AFlag))).ToList();
+            HunspellWordList.Value.Affix.Suffixes.Where(s => wordEntryDetail.Any(x => x.ContainsFlag(s.AFlag)))
+                .ToList();
 
         foreach (var suffixEntry in allSuffixesForWord.SelectMany(s => s.Entries))
             if (TryAppend(suffixEntry, word, out combined))
                 yield return combined;
 
-        foreach (var prefixEntry in allPrefixesForWord.Where(p => p.AllowCross).SelectMany(p => p.Entries))
+        foreach (var prefixEntry in allPrefixesForWord.Where(p => p.Options.HasFlag(AffixEntryOptions.CrossProduct))
+                     .SelectMany(p => p.Entries))
         {
             if (!TryAppend(prefixEntry, word, out var withPrefix)) continue;
-            foreach (var suffixEntry in allSuffixesForWord.Where(s => s.AllowCross).SelectMany(s => s.Entries))
-                if (TryAppend(suffixEntry, withPrefix!, out combined))
+            foreach (var suffixEntry in allSuffixesForWord.Where(s => s.Options.HasFlag(AffixEntryOptions.CrossProduct))
+                         .SelectMany(s => s.Entries))
+                if (TryAppend(suffixEntry, withPrefix, out combined))
                     yield return combined;
         }
     }
